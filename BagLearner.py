c46feae6b55d9b1abf0fcda03afcbc7978f4c596
jielyugt <jie.lyu@gatech.edu>
2019-09-22 21:10:30 -0400

    BagLearner initiated

---
+++ BagLearner.py
@@ -0,0 +1,23 @@
+import numpy as np  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+class BagLearner(object):  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+    def __init__(self, learner, bags, kwargs, boost, verbose):  		   	  			  	 		  		  		    	 		 		   		 		  
+        self.learner = learner
+        self.bags = bags
+        self.kwargs = kwargs
+        self.boost = boost
+        self.verbose = verbose 		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+    def author(self):  		   	  			  	 		  		  		    	 		 		   		 		  
+        return 'jlyu31'
+  		   	  			  	 		  		  		    	 		 		   		 		  
+    def addEvidence(self,dataX,dataY):  		   	  			  	 		  		  		    	 		 		   		 		  
+        print('not implemented')  		   	  			  	 		  		  		    	 		 		   		 		  
+
+    def query(self,points):  		   	  			  	 		  		  		    	 		 		   		 		  
+        print('not implemented')  		   	  			  	 		  		  		    	 		 		   		 		  
+         		   	  			  	 		  		  		    	 		 		   		 		  
+		  	 		  		  		    	 		 		   		 		  
+if __name__=="__main__":  		   	  			  	 		  		  		    	 		 		   		 		  
+    print('not implemented')
